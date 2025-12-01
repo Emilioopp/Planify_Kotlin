f91050e863fffc2e80c7231aaa74a0e8679dc280
EmilioAlarconGomez <emilio.alarcon.gomez@gmail.com>
2025-11-30 23:29:18 -0300

    Fix bug de vistas de sesiones de hoy y proximas, se hizo una sola vista para ambos, Crud de Evaluaciones terminado y funcionando.

--- app/src/main/java/com/example/planify/PlanifyApp.kt
+++ app/src/main/java/com/example/planify/PlanifyApp.kt
@@ -1,43 +1,51 @@
 package com.example.planify
 
 import android.app.Application
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.ViewModelStoreOwner
-import androidx.lifecycle.ViewModelProvider
+import com.example.planify.model.Sesion
+import com.example.planify.utils.FechaUtils
+import com.example.planify.viewmodel.SesionesViewModel
 
 class PlanifyApp : Application(), ViewModelStoreOwner {
-
     private lateinit var appViewModelStore: ViewModelStore
     lateinit var sesionesViewModel: SesionesViewModel
-    lateinit var sesionesProximasViewModel: SesionesProximasViewModel
 
     override fun onCreate() {
         super.onCreate()
 
+        // Crear el ViewModelStore para la aplicación
         appViewModelStore = ViewModelStore()
 
         val factory = ViewModelProvider.AndroidViewModelFactory.getInstance(this)
 
+        // Crear la instancia del ViewModel
         sesionesViewModel =
             ViewModelProvider(this, factory).get(SesionesViewModel::class.java)
-
-        sesionesProximasViewModel =
-            ViewModelProvider(this, factory).get(SesionesProximasViewModel::class.java)
 
         instancia = this
     }
 
-    override val viewModelStore: ViewModelStore
+    override val viewModelStore: ViewModelStore // Proporcionar el ViewModelStore
         get() = appViewModelStore
 
     companion object {
-        lateinit var instancia: PlanifyApp
+        lateinit var instancia: PlanifyApp // Instancia de la aplicación
             private set
 
-        val sesionesViewModelGlobal: SesionesViewModel
+        val sesionesViewModelGlobal: SesionesViewModel // Acceder al ViewModel desde otras clases
             get() = instancia.sesionesViewModel
+    }
 
-        val sesionesProximasViewModelGlobal: SesionesProximasViewModel
-            get() = instancia.sesionesProximasViewModel
+    fun moverSesionSiCambiaDeCategoria(original: Sesion, nueva: Sesion) {
+        val hoy = FechaUtils.formatearHoy()
+        val esOriginalHoy = original.fecha == hoy
+        val esNuevaHoy = nueva.fecha == hoy
+        val vm = sesionesViewModelGlobal
+
+        vm.eliminarSesion(original) // Eliminar la sesion original
+
+        vm.agregarSesion(nueva) // Agregar la sesion nueva
     }
 }
--- app/src/main/java/com/example/planify/Sesion.kt
+++
@@ -1,8 +0,0 @@
-package com.example.planify
-
-data class Sesion(
-    val nombre: String,
-    val fecha: String,
-    val hora: String,
-    val duracion: String
-)
